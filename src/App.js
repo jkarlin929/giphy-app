de291af4d8575a23278d069d3d6624addc6dde9a
jkarlin929 <osida11572@gmail.com>
2019-02-12 20:52:32 -0500

    remove env and add to componentdidmount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
-import getGifs from './services/service-worker';
+import getGifs from './services/api-helpers';
 import KittenList from './components/KittenList';
 import KittenListItem from './components/KittenListItem';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Banana Cat Giferizer',
+      kittenData: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const kittens = await getGifs();
+      console.log(kittens.data.data);
+      this.setState({
+        kittenData: kittens.data.data
+      })
+    }
+    catch(error) {
+      console.error(error)
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello Friends, I am an app component</h1>
+        <h1>{this.state.message}</h1>
         <KittenList />
         <KittenListItem />
       </div>
--- src/services/api-helpers.js
+++ src/services/api-helpers.js
@@ -4,7 +4,7 @@ const BASE_URL = 'http://api.giphy.com/v1/gifs/search';
 
 async function getGifs() {
   try {
-    const gifs = await axios(`${BASE_URL}?api_key=${process.env.REACT_APP_API_KEY}&q=kittens`);
+    const gifs = await axios(`${BASE_URL}?api_key=klwWTXmiAtGIQ3OLe3WieDqYBziOTK4M&q=kittens`);
     return gifs;
   }
   catch(error) {
